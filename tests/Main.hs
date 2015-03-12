16ad07b3e9a6a8c87ee13ab5580d033e4bcef08b
Philipp Volugine <phil.volguine@gmail.com>
2015-03-12 19:00:15 -0400

    added new test (prop_size)

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,6 +1,7 @@
 import Bucket
-import qualified Data.Map as Map
 
+import qualified Data.Map as Map
+import Data.List (nub)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
 import Test.Framework (defaultMain, testGroup, Test)
 
@@ -9,10 +10,18 @@ prop_fits bid size bmin bmax value =
     fits value bucket == fits value (split bucket)
     where bucket = Bucket bid size bmin bmax Map.empty
 
+prop_size :: Int -> [Int] -> Bool
+prop_size bid xs = bsize (foldr (\j b -> insert j j b) bucket (nub xs)) == len
+    where len = length $ nub xs
+          bucket = Bucket bid len minBound maxBound Map.empty
+          bsize (Split a b) = bsize a + bsize b
+          bsize (Bucket _ _ _ _ bMap) = Map.size bMap
+
 tests :: [Test]
 tests = [
         testGroup "QuickCheck Bucket" [
-            testProperty "Fits into bucket and split" prop_fits
+            testProperty "Fits into bucket and split"              prop_fits,
+            testProperty "Always store things in boundless bucket" prop_size
             ]
         ]
 
