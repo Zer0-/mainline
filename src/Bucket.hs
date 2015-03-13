8cde0595c6f443ed03411289c7f1acf379309628
Philipp Volugine <phil.volguine@gmail.com>
2015-03-12 22:02:43 -0400

    generalized the route table data definition so we can use it with Words

--- src/Bucket.hs
+++ src/Bucket.hs
@@ -2,38 +2,26 @@ module Bucket where
 
 import qualified Data.Map as Map
 
---                           +-------------ID
---                           |   +---------Bucket Size
---                           |   |   +-----Min distance
---                           |   |   |   +-Max distance
---                           |   |   |   |
-data RoutingTable v = Bucket Int Int Int Int (Map.Map Int v)
-    | Split (RoutingTable v) (RoutingTable v)
+--                             +---------ID
+--                             | +-------Bucket Size
+--                             | |   +---Min Value
+--                             | |   | +-Max value
+--                             | |   | |
+data RoutingTable a v = Bucket a Int a a (Map.Map a v)
+    | Split (RoutingTable a v) (RoutingTable a v)
 
-bucketMin :: RoutingTable v -> Int
-bucketMin (Split a _) = bucketMin a
-bucketMin (Bucket _ _ bmin _ _) = bmin
-
-bucketMax :: RoutingTable v -> Int
-bucketMax (Split _ b) = bucketMax b
-bucketMax (Bucket _ _ _ bmax _) = bmax
-
-bucketID :: RoutingTable v -> Int
-bucketID (Split a _) = bucketID a
-bucketID (Bucket bid _ _ _ _) = bid
-
-fits :: Int -> RoutingTable v -> Bool
-fits i (Split a b) = (bucketMin a <= i) && (i < bucketMax b)
+fits :: Ord a => a -> RoutingTable a v -> Bool
+fits i (Split a b) = fits i a || fits i b
 fits i (Bucket _ _ bmin bmax _) = bmin <= i && i < bmax
 
-split :: RoutingTable v -> RoutingTable v
+split :: Integral a => RoutingTable a v -> RoutingTable a v
 split (Bucket bid bsize bmin bmax bmap) =
     Split (Bucket bid bsize bmin mid less) (Bucket bid bsize mid bmax more)
     where mid = (bmin + bmax) `quot` 2
           (less, more) = Map.partitionWithKey (\k _ -> bmin <= k && k < mid) bmap
 split rt = rt
 
-insert :: Int -> v -> RoutingTable v -> RoutingTable v
+insert :: (Ord a, Integral a) => a -> v -> RoutingTable a v -> RoutingTable a v
 insert key value (Split a b)
     | fits key a = insert key value a
     | otherwise = insert key value b
