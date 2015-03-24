18108d5437f4e0ced0a9ddd5da396498822e4461
Philipp Volugine <phil.volguine@gmail.com>
2015-03-23 21:05:46 -0400

    Slight test improvements

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,22 +1,24 @@
 import Bucket
+import KRPC
 
-import qualified Data.Map as Map
-import Data.List (nub)
-import Data.Word (Word32)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
 import Test.Framework (defaultMain, testGroup, Test)
+
+import Data.List (nub)
+import Data.Word (Word32)
+import qualified Data.Map as Map
 
 prop_fits :: Word32 -> Int -> Word32 -> Word32 -> Word32 -> Bool
 prop_fits bid size bmin bmax value =
     fits value bucket == fits value (split bucket)
     where bucket = Bucket bid size bmin bmax Map.empty
 
-prop_size :: Int -> [Int] -> Bool
+prop_size :: Word32 -> [Word32] -> Bool
 prop_size bid xs = bsize (foldr (\j b -> insert j j b) bucket (nub xs)) == len
     where len = length $ nub xs
           bucket = Bucket bid len minBound maxBound Map.empty
           bsize (Split a b) = bsize a + bsize b
-          bsize (Bucket _ _ _ _ bMap) = Map.size bMap
+          bsize b = Map.size $ bucketData b
 
 tests :: [Test]
 tests = [
