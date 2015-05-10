fa7d9740e0fe83ecadacfd81f9c15517c78188b7
Philipp Volugine <phil.volguine@gmail.com>
2015-05-10 18:45:49 -0400

    begin implementing Message being BEncodable

--- src/KRPC.hs
+++ src/KRPC.hs
@@ -1,5 +1,11 @@
 module KRPC where
 
+import Data.BEncode
+import Data.BEncode.BDict
+
+import Data.ByteString (ByteString)
+import Data.ByteString.Char8 (pack)
+
 type NodeID = Int --Word160
 
 type InfoHash = Int --Word160
@@ -15,14 +21,51 @@ type NodeInfo = (NodeID, CompactInfo)
 type Token = [Int] -- [Word8]
 
 data Message
-    = Query Message
-    | Response Message
-    | Ping NodeID
-    | FindNode { queryingNodeId :: NodeID, targetNodeId :: NodeID }
-    | NodeFound NodeID (Either NodeInfo [NodeInfo])
-    | AskPeers NodeID InfoHash
-    | PeersFound NodeID Token Message
+    = Query NodeID Message
+    | Response NodeID Message
+    | Ping
+    | FindNode NodeID
+    | NodeFound (Either NodeInfo [NodeInfo])
+    | AskPeers InfoHash
+    | PeersFound Token Message
     | Values [CompactInfo]
     | Nodes [NodeInfo]
-    | AnnouncePeer NodeID Port Token Bool -- last arg is implied_port
-    | Error
+    | AnnouncePeer Port Token Bool -- last arg is implied_port
+    | Error { errCode :: Integer, errMsg :: String }
+
+bd :: String -> String -> BDictMap BValue
+bd a b = singleton (pack a) (BString $ pack b)
+
+msgName :: Message -> String
+msgName Ping = "ping"
+msgName (FindNode _) = "find_node"
+msgName (AskPeers _) = "get_peers"
+msgName (AnnouncePeer _ _ _) = "announce_peer"
+msgName _ = undefined
+
+msgToBDictMap :: Message -> BDictMap BValue
+msgToBDictMap (Error code msg) = bd "y" "e"
+    `union` singleton (pack "e") (BList [BInteger code, BString (pack msg)])
+
+msgToBDictMap (Query i m) = bd "y" "q"
+    `union` bd "q" (msgName m)
+    `union` singleton (pack "a")
+           (BDict (singleton (pack "id") (toBEncode i) `union` msgToBDictMap m))
+
+msgToBDictMap Ping = Nil
+
+msgToBDictMap _ = undefined
+
+data KPacket = KPacket
+    { transactionId :: ByteString
+    , message       :: Message
+    }
+
+instance BEncode KPacket where
+    toBEncode (KPacket { transactionId = t, message = m }) =
+        BDict $ singleton (pack "t") (BString t) `union` (msgToBDictMap m)
+
+    fromBEncode _ = undefined
+
+testBDict :: BValue
+testBDict = BDict $ Cons (pack "A") (BInteger 1) (Cons (pack "B") (BInteger 2) Nil)
