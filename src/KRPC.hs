57721fac0a3157a8e4a6ec42b51e75e1bc12f3f4
Philipp Volugine <phil.volguine@gmail.com>
2015-05-24 22:11:34 -0400

    KRPC now using real world data types

--- src/KRPC.hs
+++ src/KRPC.hs
@@ -1,24 +1,70 @@
 module KRPC where
 
+import Data.Word
+import Data.Bits
+import Data.ByteString (pack, ByteString)
+import qualified Data.ByteString.Char8 as Char8
+import Data.Digest.SHA1 (Word160 (Word160))
 import Data.BEncode
 import Data.BEncode.BDict
 
-import Data.ByteString (ByteString)
-import Data.ByteString.Char8 (pack)
+type NodeID = Word160
 
-type NodeID = Int --Word160
+type InfoHash = Word160
 
-type InfoHash = Int --Word160
+type Port = Word16
 
-type IP = Int
+data CompactInfo = CompactInfo
+    { ip    :: Word32
+    , port  :: Port
+    }
 
-type Port = Int
+data NodeInfo = NodeInfo
+    { nodeId       :: NodeID
+    , compactInfo  :: CompactInfo
+    }
 
-type CompactInfo = (IP, Port)
+type Token = ByteString
 
-type NodeInfo = (NodeID, CompactInfo)
+stringpack :: String -> ByteString
+stringpack = Char8.pack
 
-type Token = [Int] -- [Word8]
+class Octets a where
+    octets :: a -> [Word8]
+
+instance Octets Word16 where
+    octets w =
+        [ fromIntegral (w `shiftR` 8)
+        , fromIntegral w
+        ]
+
+instance Octets Word32 where
+    octets w =
+        [ fromIntegral (w `shiftR` 24)
+        , fromIntegral (w `shiftR` 16)
+        , fromIntegral (w `shiftR` 8)
+        , fromIntegral w
+        ]
+
+instance Octets Word160 where
+    octets (Word160 a1 a2 a3 a4 a5) = octets a1  ++ octets a2 ++ octets a3 ++ octets a4 ++ octets a5
+
+instance Octets CompactInfo where
+    octets (CompactInfo i p) = octets i ++ octets p
+
+instance Octets NodeInfo where
+    octets (NodeInfo nId nInfo) = octets nId ++ octets nInfo
+
+bEncode :: (Octets a) => a -> BValue
+bEncode = toBEncode . pack . octets
+
+instance BEncode CompactInfo where
+    toBEncode = bEncode
+    fromBEncode = undefined
+
+instance BEncode NodeInfo where
+    toBEncode = bEncode
+    fromBEncode = undefined
 
 data Message
     = Query NodeID Message
@@ -34,34 +80,36 @@ data Message
     | Error { errCode :: Integer, errMsg :: String }
 
 bd :: String -> String -> BDictMap BValue
-bd a b = singleton (pack a) (BString $ pack b)
+bd a b = singleton (stringpack a) (BString $ stringpack b)
 
 msgName :: Message -> String
-msgName Ping = "ping"
-msgName (FindNode _) = "find_node"
-msgName (AskPeers _) = "get_peers"
+msgName  Ping                = "ping"
+msgName (FindNode _        ) = "find_node"
+msgName (AskPeers _        ) = "get_peers"
 msgName (AnnouncePeer _ _ _) = "announce_peer"
-msgName _ = undefined
+msgName _                    = undefined
 
 msgToBDictMap :: Message -> BDictMap BValue
 msgToBDictMap (Error code msg) = bd "y" "e"
-    `union` singleton (pack "e") (BList [BInteger code, BString (pack msg)])
+    `union` singleton (stringpack "e") (BList [BInteger code, BString (stringpack msg)])
 
 msgToBDictMap (Query i m) = bd "y" "q"
     `union` bd "q" (msgName m)
-    `union` singleton (pack "a")
-           (BDict (singleton (pack "id") (toBEncode i) `union` msgToBDictMap m))
+    `union` singleton (stringpack "a")
+           (BDict (singleton (stringpack "id") (bEncode i) `union` msgToBDictMap m))
 
 msgToBDictMap (Response i m) = bd "y" "r"
-    `union` singleton (pack "r")
-           (BDict $ singleton (pack "id") (toBEncode i) `union` msgToBDictMap m)
+    `union` singleton (stringpack "r")
+           (BDict $ singleton (stringpack "id") (bEncode i) `union` msgToBDictMap m)
 
 msgToBDictMap Ping = Nil
 
-msgToBDictMap (FindNode n) = singleton (pack "target") (toBEncode n)
+msgToBDictMap (FindNode i) = singleton (stringpack "target") (bEncode i)
 
-msgToBDictMap (Nodes (n : [])) = singleton (pack "nodes") (toBEncode n)
-msgToBDictMap (Nodes n       ) = singleton (pack "nodes") (toBEncode n)
+msgToBDictMap (Nodes (n : [])) = singleton (stringpack "nodes") (toBEncode n)
+msgToBDictMap (Nodes n       ) = singleton (stringpack "nodes") (toBEncode n)
+
+msgToBDictMap (AskPeers i) = singleton (stringpack "info_hash") (bEncode i)
 
 msgToBDictMap _ = undefined
 
@@ -72,9 +120,18 @@ data KPacket = KPacket
 
 instance BEncode KPacket where
     toBEncode (KPacket { transactionId = t, message = m }) =
-        BDict $ singleton (pack "t") (BString t) `union` (msgToBDictMap m)
+        BDict $ singleton (stringpack "t") (BString t) `union` (msgToBDictMap m)
 
     fromBEncode _ = undefined
 
+{-
 testKPacket :: KPacket
-testKPacket = KPacket (pack "transaction") (Query 1337 Ping)
+testKPacket = KPacket (stringpack "transaction") (Query (Word160 0 0 0 0 1337) Ping)
+-}
+
+testKPacket :: KPacket
+testKPacket = KPacket (stringpack "transaction2") (
+    Nodes [ NodeInfo (Word160 1 1 1 1 1337) (CompactInfo 192 8080)
+          , NodeInfo (Word160 2 2 2 2 1338) (CompactInfo 168 9999)
+          ]
+    )
--- tests/Main.hs
+++ tests/Main.hs
@@ -8,6 +8,8 @@ import Data.List (nub)
 import Data.Word (Word32)
 import qualified Data.Map as Map
 
+import Data.BEncode (toBEncode, encode)
+
 prop_fits :: Word32 -> Int -> Word32 -> Word32 -> Word32 -> Bool
 prop_fits bid size bmin bmax value =
     fits value bucket == fits value (split bucket)
@@ -29,4 +31,7 @@ tests = [
         ]
 
 main :: IO ()
-main = defaultMain tests
+main = do
+    --defaultMain tests
+    putStrLn $ show $ (toBEncode testKPacket)
+    putStrLn $ show $ (encode testKPacket)
