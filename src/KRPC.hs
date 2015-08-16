2336f5ccfac464c5e250e6dd59ec073f36383503
Philipp Volugine <phil.volguine@gmail.com>
2015-08-16 14:12:14 -0400

    Decoding ping implemented
    -decoding node ids to Word160
    -added trivial Ping decoding test

--- src/KRPC.hs
+++ src/KRPC.hs
@@ -2,7 +2,8 @@ module KRPC where
 
 import Data.Word
 import Data.Bits
-import Data.ByteString (pack, ByteString)
+import Data.List (foldl')
+import Data.ByteString (pack, unpack, ByteString)
 import qualified Data.ByteString.Char8 as Char8
 import Data.Digest.SHA1 (Word160 (Word160))
 import Data.BEncode
@@ -56,13 +57,18 @@ instance Octets Word32 where
         ]
 
 instance Octets Word160 where
-    octets (Word160 a1 a2 a3 a4 a5) = octets a1  ++ octets a2 ++ octets a3 ++ octets a4 ++ octets a5
+    octets (Word160 a1 a2 a3 a4 a5) = octets a1 ++ octets a2 ++ octets a3 ++ octets a4 ++ octets a5
 
 instance Octets CompactInfo where
     octets (CompactInfo i p) = octets i ++ octets p
 
 instance Octets NodeInfo where
     octets (NodeInfo nId nInfo) = octets nId ++ octets nInfo
+
+fromOctets :: [Word8] -> Word32
+fromOctets = foldl' accum 0
+  where
+    accum a o = (a `shiftL` 8) .|. fromIntegral o
 
 bEncode :: (Octets a) => a -> BValue
 bEncode = toBEncode . pack . octets
@@ -90,7 +96,11 @@ data Message
 
 instance Show Message where
     show (Error code msg) = "Error{code: " ++ show code
-                                    ++ " msg: " ++ show msg ++ "}"
+                                    ++ ", msg: " ++ show msg ++ "}"
+
+    show (Query nid msg) = "Query<from: " ++ show nid ++ ">{"
+                                        ++ show msg ++ "}"
+    show Ping = "Ping"
     show _ = ""
 
 instance Eq Message where
@@ -110,6 +120,24 @@ instance Eq Message where
 bd :: String -> String -> BDictMap BValue
 bd a b = singleton (stringpack a) (BString $ stringpack b)
 
+bs_y :: ByteString
+bs_y = stringpack "y"
+
+bs_e :: ByteString
+bs_e = stringpack "e"
+
+bs_q :: ByteString
+bs_q = stringpack "q"
+
+bs_a :: ByteString
+bs_a = stringpack "a"
+
+bs_t :: ByteString
+bs_t = stringpack "t"
+
+bs_id :: ByteString
+bs_id = stringpack "id"
+
 msgName :: Message -> String
 msgName  Ping                  = "ping"
 msgName (FindNode _          ) = "find_node"
@@ -124,11 +152,11 @@ msgToBDictMap (Error code msg) = bd "y" "e"
 msgToBDictMap (Query queryNodeId m) = bd "y" "q"
     `union` bd "q" (msgName m)
     `union` singleton (stringpack "a")
-           (BDict (singleton (stringpack "id") (bEncode queryNodeId) `union` msgToBDictMap m))
+           (BDict (singleton bs_id (bEncode queryNodeId) `union` msgToBDictMap m))
 
 msgToBDictMap (Response respondNodeId m) = bd "y" "r"
     `union` singleton (stringpack "r")
-           (BDict $ singleton (stringpack "id") (bEncode respondNodeId) `union` msgToBDictMap m)
+           (BDict $ singleton bs_id (bEncode respondNodeId) `union` msgToBDictMap m)
 
 msgToBDictMap Ping = Nil
 
@@ -156,18 +184,31 @@ msgToBDictMap (AnnouncePeer infohash portnum token implied_port) =
 msgToBDictMap _ = undefined
 
 {-
- - >>> sorted([b'e', b'y', b't', b'q', b'r'])
- - [b'e', b'q', b'r', b't', b'y']
- - e q r t y
+ - >>> sorted([b'e', b'y', b't', b'q', b'r', b'a'])
+ - [b'a', b'e', b'q', b'r', b't', b'y']
+ - a e q r t y
  -}
 
-bs_y :: ByteString
-bs_y = stringpack "y"
-
-bs_e :: ByteString
-bs_e = stringpack "e"
+parseNodeID :: ByteString -> NodeID
+parseNodeID bs = Word160 a b c d e
+    where bytes = unpack bs
+          a = fromOctets $ take 32 bytes
+          b = fromOctets $ take 32 (drop 32 bytes)
+          c = fromOctets $ take 32 (drop 64 bytes)
+          d = fromOctets $ take 32 (drop 96 bytes)
+          e = fromOctets $ take 32 (drop 128 bytes)
 
 bDictMapToMsg :: BDictMap BValue -> Message
+bDictMapToMsg (Cons a (BDict (Cons i (BString nid) Nil))
+                (Cons _ (BString qval)
+                    (Cons y (BString yval) Nil)))
+    |  y == bs_y
+    && yval == bs_q
+    && qval == stringpack "ping"
+    && a == bs_a
+    && i == bs_id
+    = Query (parseNodeID nid) Ping
+
 bDictMapToMsg (Cons
                 e
                 (BList ((BInteger code) : (BString msg) : []))
@@ -187,6 +228,16 @@ instance Show KPacket where
 instance BEncode KPacket where
     toBEncode (KPacket { transactionId = t, message = m }) =
         BDict $ singleton (stringpack "t") (BString t) `union` (msgToBDictMap m)
+
+    fromBEncode (BDict (Cons a meat
+                            (Cons q qval
+                                (Cons t (BString tid)
+                                    (Cons y yval Nil)))))
+        |  t == bs_t
+        && q == bs_q = Right $ KPacket tid (bDictMapToMsg xs)
+            where xs = singleton a meat
+                        `union` singleton q qval
+                        `union` singleton y yval
 
     fromBEncode (BDict (Cons a meat
                             (Cons t (BString tid)
--- tests/Main.hs
+++ tests/Main.hs
@@ -5,11 +5,15 @@ import Test.Framework.Providers.QuickCheck2 (testProperty)
 import Test.Framework (defaultMain, testGroup, Test)
 
 import Data.List (nub)
-import Data.Word (Word32)
+import Data.Word (Word32, Word8)
 import qualified Data.Map as Map
+
+import Data.ByteString (pack)
 
 import Data.BEncode
 import Data.BEncode.BDict
+
+--import Debug.Trace
 
 prop_fits :: Word32 -> Int -> Word32 -> Word32 -> Word32 -> Bool
 prop_fits bid size bmin bmax value =
@@ -23,13 +27,23 @@ prop_size bid xs = bsize (foldr (\j b -> insert j j b) bucket (nub xs)) == len
           bsize (Split a b) = bsize a + bsize b
           bsize b = Map.size $ bucketData b
 
-fmt_decodeErr :: String -> Integer -> String -> Bool
+fmt_decodeErr :: [Word8] -> Integer -> String -> Bool
 fmt_decodeErr i code msg = fromBEncode bval == (Right $ KPacket tid (Error code msg))
     where bval = BDict $
                     (singleton (stringpack "t") (BString tid))
                     `union` bd "y" "e"
                     `union` (singleton (stringpack "e") (BList [BInteger code, BString (stringpack msg)]))
-          tid = stringpack i
+          tid = pack i
+
+fmt_decodePing :: [Word8] -> [Word8] -> Bool
+fmt_decodePing i n = fromBEncode bval == (Right $ KPacket tid (Query (parseNodeID nid) Ping))
+    where bval = BDict $
+                    singleton bs_a (BDict (singleton bs_id (BString nid)))
+                    `union` (singleton bs_t (BString tid))
+                    `union` bd "q" "ping"
+                    `union` bd "y" "q"
+          tid = pack i
+          nid = pack n
 
 tests :: [Test]
 tests = [
@@ -38,7 +52,8 @@ tests = [
             testProperty "Always store things in boundless bucket" prop_size
             ],
         testGroup "KRPC Sanity" [
-            testProperty "decode error message"                    fmt_decodeErr
+            testProperty "decode error message"                    fmt_decodeErr,
+            testProperty "decode Ping  message"                    fmt_decodePing
             ]
         ]
 
