649b101b461805370f84d295cae7a9a4b1d13dda
Philipp Volguine <phil.volguine@gmail.com>
2017-05-06 21:42:10 -0400

    Ping seems to work!

--- src/App.hs
+++ src/App.hs
@@ -2,14 +2,12 @@
  - Testing sending and receiving a single UDP packet
  -}
 
---import KRPC
 import Network.Socket
     ( socket
-    , sendTo
-    , recvFrom
+--  , sendTo
 --  , bind
     , Family(AF_INET)
-    , HostAddress
+--- , HostAddress
     , SocketType(Datagram)
     , SockAddr(..)
     , AddrInfo(..)
@@ -18,8 +16,15 @@ import Network.Socket
 --  , iNADDR_ANY
     )
 
-targetPort = "49000"
-targetHost = "127.0.0.1"
+import Network.Socket.ByteString (sendTo, recvFrom)
+
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as BL
+import Data.BEncode (encode)
+import KRPC
+
+targetPort = "51413"
+targetHost = "192.168.4.11"
 --MAXLINE = 65507 -- Max size of a UDP datagram
 --(limited by 16 bit length part of the header field)
 maxline = 2048
@@ -33,18 +38,23 @@ portFromSockAddr (SockAddrInet p _) = show p
 portFromSockAddr (SockAddrInet6 p _ _ _) = show p
 portFromSockAddr _ = undefined
 
+ping = (BS.concat . BL.toChunks . encode)
+    $ KPacket (stringpack "hello")
+    $ Query ((fromOctets . BS.unpack . stringpack) "hello world") Ping
+
 main :: IO ()
 main = do
-    putStrLn "What would you like to send?"
-    payload <- getLine
+    --putStrLn "What would you like to send?"
+    --payload <- getLine
     sock           <- socket AF_INET Datagram 0
     addrInfoList   <- getAddrInfo Nothing (Just targetHost) (Just targetPort)
     let targetInetAddr = addrAddress (head addrInfoList)
-    _ <- sendTo sock payload targetInetAddr
-    (response, nBytesRecv, fromSocketAddr) <- recvFrom sock maxline
+    _ <- sendTo sock ping targetInetAddr
+    putStrLn $ "sent" ++ (show ping)
+    (response, fromSocketAddr) <- recvFrom sock maxline
     fromHost <- hostFromSockAddr fromSocketAddr
     let fromPort = portFromSockAddr fromSocketAddr
     putStrLn $ "RESPONSE ("
-                ++ (show nBytesRecv)
+                ++ (show $ BS.length response)
                 ++ " bytes from " ++ (show fromHost) ++ ":" ++ (show fromPort) ++ ")\n"
-                ++ response
+                ++ show response
