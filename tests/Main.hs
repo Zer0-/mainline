f65174089cde28dde8f9f697b1b4f335325d4bee
Philipp Volguine <phil.volguine@gmail.com>
2017-05-11 02:04:40 -0400

    Simplified Octets class tests on Words
    -Using [Word8] directly instead of packing/unpacking ByteString
    -Can get rid of Arbitrary Bytestring instance and compiler warning now

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,28 +1,20 @@
-{-# OPTIONS_GHC -fno-warn-orphans #-}
-
 import Bucket
 import KRPC
 
 import Test.Framework.Providers.QuickCheck2 (testProperty)
 import Test.Framework (defaultMain, testGroup, Test)
-import Test.QuickCheck (Arbitrary, arbitrary)
 
 import Data.List (nub)
 import Data.Word (Word32, Word16, Word8)
 import qualified Data.Map as Map
 
-import Data.ByteString (ByteString, pack, unpack)
+import Data.ByteString (pack)
 
 import Data.BEncode
 import Data.BEncode.BDict hiding (map)
 import Data.Digest.SHA1 (Word160)
 
 --import Debug.Trace
-
-instance Arbitrary ByteString where
-    arbitrary = do
-        ws <- arbitrary
-        return (pack ws)
 
 prop_fits :: Word32 -> Int -> Word32 -> Word32 -> Word32 -> Bool
 prop_fits bid size bmin bmax value =
@@ -36,26 +28,23 @@ prop_size bid xs = bsize (foldr (\j b -> insert j j b) bucket (nub xs)) == len
           bsize (Split a b) = bsize a + bsize b
           bsize b = Map.size $ bucketData b
 
-word16_bytestring_bijection :: ByteString -> Bool
-word16_bytestring_bijection b = bytes == pack (octets w)
+word16_bytestring_bijection :: [Word8] -> Bool
+word16_bytestring_bijection b = padded == (octets . word16FromOctets) b
     where
-        w :: Word16
-        w = fromByteString b
-        bytes = pack $ take 2 (extendListWith (unpack b) 0)
+        padded = take 2 (extendListWith b 0)
+        word16FromOctets = fromOctets :: [Word8] -> Word16
 
-word32_bytestring_bijection :: ByteString -> Bool
-word32_bytestring_bijection b = bytes == pack (octets w)
+word32_bytestring_bijection :: [Word8] -> Bool
+word32_bytestring_bijection b = padded == (octets . word16FromOctets) b
     where
-        w :: Word32
-        w = fromByteString b
-        bytes = pack $ take 4 (extendListWith (unpack b) 0)
+        padded = take 4 (extendListWith b 0)
+        word16FromOctets = fromOctets :: [Word8] -> Word32
 
-word160_bytestring_bijection :: ByteString -> Bool
-word160_bytestring_bijection b = bytes == pack (octets w)
+word160_bytestring_bijection :: [Word8] -> Bool
+word160_bytestring_bijection b = padded == (octets . word16FromOctets) b
     where
-        w :: Word160
-        w = fromByteString b
-        bytes = pack $ take 20 (extendListWith (unpack b) 0)
+        padded = take 20 (extendListWith b 0)
+        word16FromOctets = fromOctets :: [Word8] -> Word160
 
 fmt_decodeErr :: [Word8] -> Integer -> String -> Bool
 fmt_decodeErr i code msg = fromBEncode bval == (Right $ KPacket tid (Error code msg))
