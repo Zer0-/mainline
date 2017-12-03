e26e5239ba7a7dc50584defa1dba0bee19a40125
Philipp Volguine <phil.volguine@gmail.com>
2017-12-03 01:41:09 -0500

    Server mainloop sketched out
    -Notes
    -Loosely defined functions
    -Ord instance for Word160 for bucket compatability

---
+++ src/Network/KRPC/WordInstances.hs
@@ -0,0 +1,9 @@
+module Network.KRPC.WordInstances where
+
+import Prelude hiding   (toInteger)
+import Data.Function    (on)
+import Data.Digest.SHA1 (Word160, toInteger)
+
+{-# MINIMAL compare #-}
+instance Ord Word160 where
+    compare = on compare toInteger
