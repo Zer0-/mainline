a5d70137ac4084f4929f17530e9dc7cae7664016
Philipp Volguine <phil.volguine@gmail.com>
2019-04-25 04:51:34 -0400

    untested query parser

--- src/Network/KRPC.hs
+++ src/Network/KRPC.hs
@@ -6,9 +6,9 @@ module Network.KRPC
 import Data.BEncode (BValue (..), BEncode (..))
 import Data.BEncode.BDict (BDictMap (..), singleton, empty, union)
 import qualified Data.ByteString as BS
-import Text.Parsec.Prim (runP, Parsec, tokenPrim)
+import Text.Parsec.Prim (runP, Parsec, tokenPrim, (<|>), try)
 import Text.Parsec.Pos (incSourceColumn)
-import Text.Parsec.Combinator (between)
+import Text.Parsec.Combinator (between, option)
 
 import Network.Octets (fromByteString, octToByteString)
 import Network.KRPC.Types
@@ -56,108 +56,33 @@ instance Show KPacket where
 instance BEncode KPacket where
     toBEncode (KPacket { transactionId = t, message = m, version = mv }) =
         BDict $
-            singleton bs_t (BString t) `union`
-            maybe empty (\v -> singleton bs_v (BString v)) mv `union`
-            (msgToBDictMap m)
+            singleton bs_t (BString t)
+            `union` maybe empty (\v -> singleton bs_v (BString v)) mv
+            `union` (msgToBDictMap m)
 
     fromBEncode bvalue
         = either
             (Left . show)
-            (Right . id)
+            Right
             (runP kparser () "Inbound" (scanner bvalue))
-
-    -- Parse query KPacket
-    {-
-    fromBEncode (BDict (Cons a meat
-                       (Cons q qval
-                       (Cons t (BString tid)
-                       (Cons y yval Nil)))))
-        |  t == bs_t && q == bs_q =
-            case bDictMapToMsg xs of
-                Left  e -> Left e
-                Right x -> Right $ KPacket tid x Nothing
-            where xs = singleton a meat
-                    `union` singleton q qval
-                    `union` singleton y yval
-    -}
-
-
-    -- Parse response KPacket
-    {-
-    fromBEncode (BDict (Cons a meat
-                       (Cons t (BString tid)
-                       (Cons y yval Nil))))
-        |  t == bs_t && y == bs_y  =
-            case bDictMapToMsg xs of
-                Left  e -> Left e
-                Right x -> Right $ KPacket tid x Nothing
-            where
-                xs = singleton a meat `union`
-                    singleton y yval
-    -}
-
-    -- Parse response KPacket containing version key
-    {-
-    fromBEncode
-        ( BDict
-            ( Cons a meat
-                ( Cons t (BString tid)
-                    ( Cons v (BString vs)
-                        ( Cons y yval Nil )
-                    )
-                )
-            )
-        )
-        |  v == bs_v && t == bs_t && y == bs_y  =
-            case bDictMapToMsg xs of
-                Left  e -> Left e
-                Right x -> Right $ KPacket tid x (Just vs)
-            where
-                xs = singleton a meat `union`
-                    singleton y yval
-    -}
-
-    -- Parse response KPacket containing ip key
-    -- (returned by certain bootstrap nodes)
-    {-
-    fromBEncode
-        ( BDict
-            ( Cons i _
-                ( Cons a meat
-                    ( Cons t (BString tid)
-                        ( Cons y yval Nil )
-                    )
-                )
-            )
-        )
-        |  i == bs_ip && t == bs_t && y == bs_y  =
-            case bDictMapToMsg xs of
-                Left  e -> Left e
-                Right x -> Right $ KPacket tid x Nothing
-            where
-                xs = singleton a meat `union`
-                    singleton y yval
-
-    fromBEncode _ = decodingError "- this doesn't look like a KRPC message"
-    -}
-
 
 
 
 msgToBDictMap :: Message -> BDictMap BValue
 msgToBDictMap (Error { errCode = code, errMsg = msg}) =
     bd "y" "e" `union`
-    singleton (stringpack "e") (BList errors)
-    where
-        errors = [BInteger code, BString msg]
+    singleton (stringpack "e") (BList [BInteger code, BString msg])
+
 
 msgToBDictMap (Query queryNodeId q) =
     bd "y" "q"           `union`
     bd "q" (queryName q) `union`
     singleton (stringpack "a") (BDict queryPayload)
+
     where
-        queryPayload = singleton bs_id (bEncode queryNodeId) `union`
-            queryToBDictMap q
+        queryPayload
+            = singleton bs_id (bEncode queryNodeId)
+            `union` queryToBDictMap q
 
 msgToBDictMap (Response respondNodeId r) =
     bd "y" "r" `union`
@@ -168,35 +93,30 @@ msgToBDictMap (Response respondNodeId r) =
             responseToBDictMap r
         )
 
+
+
 queryToBDictMap :: QueryDat -> BDictMap BValue
-
 queryToBDictMap (Ping) = Nil
-
 queryToBDictMap (FindNode i) = singleton (stringpack "target") (bEncode i)
-
 queryToBDictMap (GetPeers i) = singleton (stringpack "info_hash") (bEncode i)
-
 queryToBDictMap (AnnouncePeer implied_port infohash portnum token) =
     singleton
         (stringpack "implied_port")
-        (toBEncode $ fromEnum implied_port) `union`
-    singleton (stringpack "info_hash")    (bEncode infohash)       `union`
-    singleton (stringpack "port")         (toBEncode portnum)      `union`
-    singleton (stringpack "token")        (toBEncode token)
+        (toBEncode $ fromEnum implied_port)                `union`
+    singleton (stringpack "info_hash") (bEncode infohash)  `union`
+    singleton (stringpack "port")      (toBEncode portnum) `union`
+    singleton (stringpack "token")     (toBEncode token)
+
 
 
 responseToBDictMap :: ResponseDat -> BDictMap BValue
-
 responseToBDictMap Pong = Nil
-
 responseToBDictMap (Nodes ns) =
     singleton (stringpack "nodes") (toBEncode $ concatNodes ns)
-
 
 responseToBDictMap (PeersFound t infos) =
     singleton (stringpack "token")  (toBEncode t) `union`
     singleton (stringpack "values") (toBEncode infos)
-
 
 responseToBDictMap (NodesFound t ns) =
     singleton (stringpack "token")  (toBEncode t) `union`
@@ -225,7 +145,6 @@ parseNodes = (map fromByteString) . splitBytes
                   BS.take 26 b : splitBytes (BS.drop 26 b)
 
 
-
 type Parser a = Parsec [BVal] () a
 
 
@@ -240,24 +159,76 @@ data BVal
 
 
 kparser :: Parser KPacket
-kparser = withObject $ do
+kparser = withObject qparser >> return undefined
+
+
+qparser :: Parser Message
+qparser = do
     isBs bs_a
     withObject $ do
         isBs bs_id
         nodeid <- parseNodeid
-        return undefined
+        qd <- qdatparser
+        return $ Query nodeid qd
+
+
+qdatparser :: Parser QueryDat
+qdatparser
+    =   (FindNode <$> findNode)
+    <|> (GetPeers <$> getPeers)
+    <|> announcePeer
+    <|> return Ping
+
+    where
+        findNode
+            = isBs (stringpack "target")
+            >> try parseNodeid
+
+        getPeers
+            = isBs (stringpack "get_peers")
+            >> try parseNodeid
+
+        announcePeer = do
+            flag <- option False impliedPort
+            isBs (stringpack "info_hash")
+            try (announcePeerB flag)
+
+        announcePeerB flag = do
+            info_hash <- parseNodeid
+
+            isBs (stringpack "port")
+            port <- (fromIntegral <$> parseInt)
+
+            isBs (stringpack "token")
+            token <- parseBs
+
+            return $ AnnouncePeer flag info_hash port token
+
+        impliedPort
+            = (not . ((==) 0))
+            <$> (isBs (stringpack "implied_port") >> try parseInt)
+
+
+parseInt :: Parser Integer
+parseInt = satisfy test
+    where
+        test (BInt i) = Just i
+        test _ = Nothing
+
+
+parseBs :: Parser BS.ByteString
+parseBs = satisfy test
+    where
+        test (BBs bs) = Just bs
+        test _ = Nothing
 
 
 parseNodeid :: Parser NodeID
-parseNodeid = satisfy test
-    where
-        test (BBs nodeid) = Just $ fromByteString nodeid
-        test _ = Nothing
+parseNodeid = fromByteString <$> parseBs
 
 
 withObject :: Parser a -> Parser a
 withObject inner = between (isVal Bs) (isVal Be) inner
-
 
 
 isVal :: BVal -> Parser ()
@@ -272,7 +243,6 @@ satisfy :: (BVal -> Maybe a) -> Parser a
 satisfy test = tokenPrim show updatePos test
     where
         updatePos pos _ _ = incSourceColumn pos 1
-
 
 
 scanner :: BValue -> [BVal]
