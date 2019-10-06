19bc3eb171abaea63e269abc44408fc0bf71aadd
Philipp Volguine <phil.volguine@gmail.com>
2019-10-06 18:46:58 -0400

    Add plain sql definition of tables

---
+++ sql/sql.sql
@@ -0,0 +1,33 @@
+BEGIN TRANSACTION;
+
+CREATE TABLE IF NOT EXISTS meta_info
+    ( info_id serial
+    , info_hash bytea NOT NULL
+    , added timestamp with time zone NOT NULL DEFAULT current_timestamp
+    , score double precision NOT NULL DEFAULT 0
+    , CONSTRAINT hash_len CHECK (length(info_hash) = 20)
+    , CONSTRAINT info_pk PRIMARY KEY (info_id)
+    , CONSTRAINT info_hash_unique UNIQUE (info_hash)
+    );
+
+CREATE TABLE IF NOT EXISTS file_info
+    ( info_id integer
+    , filepath text[] NOT NULL
+    , size_bytes integer NOT NULL
+    , CONSTRAINT file_pk PRIMARY KEY (info_id, filepath)
+    , CONSTRAINT file_fk FOREIGN KEY (info_id) REFERENCES meta_info (info_id) ON DELETE CASCADE
+    );
+
+CREATE TABLE IF NOT EXISTS info_pieces
+    ( info_id integer
+    , pieces bytea
+    , CONSTRAINT pieces_divisible CHECK (length(pieces) % 20 = 0)
+    , CONSTRAINT pieces_pk PRIMARY KEY (info_id)
+    , CONSTRAINT pieces_fk FOREIGN KEY (info_id) REFERENCES meta_info (info_id) ON DELETE CASCADE
+    );
+
+DROP TABLE IF EXISTS file_info;
+DROP TABLE IF EXISTS info_pieces;
+DROP TABLE IF EXISTS meta_info;
+
+ROLLBACK;
