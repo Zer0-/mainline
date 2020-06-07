be34a624d370e57ef73381ce3bf269c37c340aba
Philipp Volguine <phil.volguine@gmail.com>
2020-06-07 15:25:22 -0400

    simple rename

--- src/Mainline/App.hs
+++ src/Mainline/App.hs
@@ -706,13 +706,13 @@ removeGettingPeers state _ = state
 
 
 calculateScore :: Int -> POSIXTime -> Double
-calculateScore n t = (euler ** ((log maxDbl / endt) * (x - t0)) - 1) * (fromIntegral n)
+calculateScore n t = (e ** ((log maxDbl / endt) * (x - t0)) - 1) * (fromIntegral n)
     where
         maxDbl = 10 ** 300
         t0 = 1572391617
         endt = t0 + (1.577 * 10**10)
         x = realToFrac t
-        euler = exp 1
+        e = exp 1
 
 throttle
     :: (Ord a)
